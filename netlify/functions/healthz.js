182a9e0d67641135bc5f80b6aa50f72a35a327f5
wt120000000 <william.thomas.120000000@gmail.com>
2025-08-22 15:56:35 -0700

    chore: engage mock opt-in, healthz function, env/CI scaffolding, publish public/

---
+++ netlify/functions/healthz.js
@@ -0,0 +1,5 @@
+exports.handler = async () => ({
+  statusCode: 200,
+  headers: { 'content-type': 'application/json' },
+  body: JSON.stringify({ status: 'ok', version: process.env.APP_VERSION || 'dev', ts: Date.now() })
+});
